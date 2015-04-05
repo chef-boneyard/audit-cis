aa7513386e1f1df977c64c453db9e1c851881122
jtimberman <joshua@chef.io>
2015-04-05 10:18:38 -0600

    metadata should have relevant URLs

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,9 @@ maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Chef audit-mode controls for CIS Benchmarks'
-version          '0.1.0'
+version          '0.1.1'
 
+unless defined?(Ridley::Chef::Cookbook::Metadata) || defined?(Stove::Cookbook::Metadata)
+  source_url       'https://github.com/chef-cookbooks/audit-cis'
+  issues_url       'https://github.com/chef-cookbooks/audit-cis/issues'
+end
