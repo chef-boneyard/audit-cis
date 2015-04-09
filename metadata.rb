1f67c4f71115d69ead8e8412dcb9ca4a49a6bdcc
jtimberman <joshua@chef.io>
2015-04-09 14:30:02 -0600

    remove guards around metadata urls

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,7 @@ maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Chef audit-mode controls for CIS Benchmarks'
-version          '0.3.1'
+version          '0.3.2'
 
-unless defined?(Ridley::Chef::Cookbook::Metadata) || defined?(Stove::Cookbook::Metadata)
-  source_url       'https://github.com/chef-cookbooks/audit-cis'
-  issues_url       'https://github.com/chef-cookbooks/audit-cis/issues'
-end
+source_url       'https://github.com/chef-cookbooks/audit-cis'
+issues_url       'https://github.com/chef-cookbooks/audit-cis/issues'
